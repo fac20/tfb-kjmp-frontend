0bbcee3623081b243eb981e7efbd019d8b309da4
Terrence <netceer112@gmail.com>
2020-10-28 00:13:13 +0000

    feat: add enter button on landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -2,8 +2,9 @@ import React from "react";
 import logo from "./../images/logo.png";
 import { Container } from "./../styled-components/Containers";
 import { Link } from "react-router-dom";
-
+import Button from "../styled-components/Button";
 import Img from "./../styled-components/Img";
+
 export default function LandingPage() {
 	return (
 		<>
@@ -11,6 +12,7 @@ export default function LandingPage() {
 				<Link to="/continents">
 					<Img src={logo} width="55vw" />
 				</Link>
+				<Button>Enter</Button>
 			</Container>
 		</>
 	);
